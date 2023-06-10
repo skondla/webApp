094514c748f96c39784e592b822bace8cb490092
skondla <kondlawork@gmail.com>
2023-06-10 19:25:50 -0400

    add linkerd service mesh configuration

--- gcp/gke/deploy/manifests/webapp/serviceMesh/setup_linkerd.sh
+++ gcp/gke/deploy/manifests/webapp/serviceMesh/setup_linkerd.sh
@@ -3,7 +3,8 @@
 curl https://run.linkerd.io/emojivoto.yml --output manifest.yaml
 cat manifest.yaml | less
 kubectl apply -f manifest.yaml
-kubectl get -n emojivoto get pods
+kubectl get -n emojivoto pods
+sleep 30
 #kubectl get -n emojivoto deploy -o yaml | linkerd inject - | kubectl apply -f -
 kubectl -n emojivoto port-forward svc/web-svc 8080:80
 # http://localhost:8080
@@ -11,7 +12,8 @@ curl https://run.linkerd.io/install | sh
 ~/.linkerd2/bin/linkerd version
 export PATH=$PATH:$HOME/.linkerd2/bin
 linkerd version
-linkerd install | kubectl apply -f -
+linkerd install --crds | kubectl apply -f -
+linkerd upgrade --crds | kubectl apply -f -
 linkerd check
 #linkerd dashboard
 linkerd stat deployments -n linkerd
