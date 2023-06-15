b9a5f6d94bcead314c218afb03f8874080fdd074
skondla <kondlawork@gmail.com>
2023-06-14 22:40:06 -0400

    Moved AWS deployment to a seperate branch :testing_aws

--- aws/createPermBoundary.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-aws cloudformation create-stack \
- --stack-name webapp1-demo-shop \
- --template-body file://${PWD}/PermissionBoundary.yaml \
- --capabilities CAPABILITY_NAMED_IAM \
- --parameters ParameterKey=DockerAuth,ParameterValue={}
