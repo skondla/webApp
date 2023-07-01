82882f9176db843d31af4e326ebcf3bd51a50cbe
Sam Kondla <kondlawork@gmail.com>
2023-06-30 20:41:56 -0400

    add kubecost setup

--- aws/eks/deploy/kubecost/install.sh
+++ aws/eks/deploy/kubecost/install.sh
@@ -1,6 +1,6 @@
 helm install kubecost cost-analyzer \
  --repo https://kubecost.github.io/cost-analyzer/ \
  --namespace kubecost --create-namespace \
- --set kubecostToken="c2tvbmRsYS5haUBnbWFpbC5jb20=xm343yadf98"
+ --set kubecostToken="${kubecostToken}"
 
 kubectl port-forward --namespace kubecost deployment/kubecost-cost-analyzer 9090
